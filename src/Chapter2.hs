19823bc589b5a72249c85b888407efe1a337ad51
Brian Shourd <brian.shourd@gmail.com>
2015-02-12 20:42:23 -0800

    Added basic unbalanced tree sets and tests

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Chapter2 where
 
 -- Exercise 2.1
 suffixes :: [a] -> [[a]]
 suffixes [] = [[]]
 suffixes xs = xs : (suffixes $ tail xs)
+
+-- Binary tree implementation
+data Tree a = E
+    | T (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+class Set s a | a -> s where
+    empty :: s a
+    insert :: s a -> a -> s a
+    member :: s a -> a -> Bool
+
+setFromList :: Set s a => [a] -> s a
+setFromList = foldl insert empty
+
+instance Ord a => Set Tree a where
+    empty = E
+
+    insert E x = T E x E
+    insert s@(T l y r) x
+        | x < y = T (insert l x) y r
+        | x > y = T l y (insert r x)
+        | otherwise = s
+
+    member E _ = False
+    member (T l y r) x
+        | x < y = member l x
+        | x > y = member r x
+        | otherwise = True
--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -15,7 +15,12 @@ main = defaultMain tests
     tests =
         [testGroup "Chapter 2"
             [testCase "Exercise 2.1: Suffixes" case_suffixes
-            ,testProperty "Exercise 2.1: Suffixes length" prop_suffixesLength
+            ,testProperty "Suffixes length is one more than number of elements" prop_suffixesLength
+            ,testProperty "Empty Set contains no elements" prop_emptySetEmpty
+            ,testProperty "Single element set contains element" prop_setContainsInsert
+            ,testProperty "Multi insert doesn't change" prop_setMultiInsert
+            ,testProperty "Multi insert contains all elements" prop_setMultiInsertContains
+            ,testProperty "Multi insert doesn't contain extras" prop_setMultiInsertOnlyContains
             ]
         ]
 
@@ -24,3 +29,20 @@ case_suffixes = suffixes [1,2,3,4] @?= ([[1,2,3,4],[2,3,4],[3,4],[4],[]] :: [[In
 
 prop_suffixesLength :: [Char] -> Bool
 prop_suffixesLength s = length (suffixes s) == (length s) + 1
+
+prop_emptySetEmpty :: Int -> Bool
+prop_emptySetEmpty x = member empty x == False
+
+prop_setContainsInsert :: Int -> Bool
+prop_setContainsInsert x = member (insert empty x) x == True
+
+prop_setMultiInsert :: Int -> Bool
+prop_setMultiInsert x = insert (insert empty x) x == insert empty x where
+
+prop_setMultiInsertContains :: [Int] -> Bool
+prop_setMultiInsertContains xs = all (member s) xs where
+    s = setFromList xs
+
+prop_setMultiInsertOnlyContains :: [Int] -> Int -> Bool
+prop_setMultiInsertOnlyContains xs y = member s y == elem y xs where
+    s = setFromList xs
