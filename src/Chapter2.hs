9b0d33da3c73f5b9b031ed8f08776c5b53a4f539
Brian Shourd <brian.shourd@gmail.com>
2015-02-14 15:18:24 -0800

    Exercises 2.3 and 2.5

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -19,8 +19,8 @@ class Set s a | a -> s where
     insert :: s a -> a -> s a
     member :: s a -> a -> Bool
 
-setFromList :: (Set Tree a) => [a] -> Tree a
-setFromList = foldl insert empty
+treeFromList :: (Set Tree a) => [a] -> Tree a
+treeFromList = foldl insert empty
 
 instance Ord a => Set Tree a where
     empty = E
@@ -65,3 +65,45 @@ betterMember' t' x' = betterMember'impl t' x' 0 Nothing where
         | x < y = (resultl, countl + 1)
         | otherwise = betterMember'impl r x (count + 1) (Just y) where
             (resultl, countl) = betterMember'impl l x count lastFalse
+
+-- Exercise 2.3
+-- A better insertion method, that doesn't change when something which is
+-- already in the set is added to the set.
+-- The book says to do it with exceptions, which is certainly more efficient
+-- than the method here. But I don't like using exceptions for controlling
+-- program flow, so here is that with
+betterInsert :: (Ord a) => Tree a -> a -> Tree a
+betterInsert s' x' = case betterInsert' s' x' of
+    Nothing -> s'
+    Just t' -> t'
+
+betterInsert' :: (Ord a) => Tree a -> a -> Maybe (Tree a)
+betterInsert' E x = Just $ T E x E
+betterInsert' (T l y r) x
+    | x < y = betterInsert' l x >>= \l' -> Just $ T l' y r
+    | x > y = betterInsert' r x >>= Just . T l y
+    | otherwise = Nothing
+
+-- Exercise 2.4
+-- Better insert and better member for Set
+-- Not actually going to do this, since there isn't much to do
+
+-- Exercise 2.5
+-- Part a - create a complete tree of depth d full of arbitrary elements
+complete :: a -> Int -> Tree a
+complete x d
+    | d <= 0 = E
+    | otherwise = T subTree x subTree where
+        subTree = complete x (d - 1)
+
+-- For testing, we need a size function
+size :: Tree a -> Int
+size E = 0
+size (T l _ r) = 1 + (size l) + (size r)
+
+-- Part b - create a tree of arbitrary elements of the given size
+balancedTree :: a -> Int -> Tree a
+balancedTree x s
+    | s <= 0 = E
+    | odd s = T (balancedTree x $ s `div` 2) x (balancedTree x $ s `div` 2)
+    | otherwise = T (balancedTree x $ (s `div` 2) - 1) x (balancedTree x $ s `div` 2)
--- tests/Chapter2Tests.hs
+++ tests/Chapter2Tests.hs
@@ -1,13 +1,16 @@
-module Chapter2Tests where
+module Chapter2Tests (chapter2TestGroup) where
 
-import Test.Framework (testGroup)
+import Test.Framework (testGroup, Test)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Test.HUnit
+import Test.QuickCheck (Gen, choose, Property, forAll)
+import Data.List (nub)
 
 import Chapter2
 
+chapter2TestGroup :: Test.Framework.Test
 chapter2TestGroup = testGroup "Chapter 2"
     [testGroup "Exercise 2.1: Suffixes"
         [testCase "Suffixes of [1,2,3,4]" case_suffixes
@@ -20,6 +23,7 @@ chapter2TestGroup = testGroup "Chapter 2"
         ,testProperty "Multi insert contains all elements" prop_setMultiInsertContains
         ,testProperty "Multi insert doesn't contain extras" prop_setMultiInsertOnlyContains
         ,testCase "depth of [1..20]" case_depthWorst
+        ,testCase "depth of [2,1,3,4]" case_depth2
         ,testProperty "depth is smaller than number of elements" prop_depthWorst
         ]
     ,testGroup "Exercise 2.2: Fewer comparisons for member function"
@@ -28,6 +32,17 @@ chapter2TestGroup = testGroup "Chapter 2"
         ,testProperty "Multi insert contains all elements, betterMember'" prop_setMultiInsertContainsBetterMember'
         ,testProperty "Multi insert doesn't contain extras, betterMember'" prop_setMultiInsertOnlyContainsBetterMember'
         ,testProperty "betterMember' uses fewer than 2d + 1 comparisons (main problem)" prop_betterMember'IsBetter
+        ]
+    ,testGroup "Exercise 2.3: A better insert function"
+        [testProperty "Multi betterInsert contains all elements" prop_setMultiBetterInsertContains
+        ,testProperty "Multi betterInsert doesn't contain extras" prop_setMultiBetterInsertOnlyContains
+        ]
+    ,testGroup "Exercise 2.5: Building balanced trees"
+        [testProperty "Size of a tree from a list is the number of uniqe elements in the list" prop_sizeTreeFromList
+        ,testProperty "complete has correct depth" prop_completeCorrectDepth
+        ,testProperty "complete has correct size" prop_completeCorrectSize
+        ,testProperty "balancedTree has correct size" prop_balancedTreeCorrectSize
+        ,testProperty "balancedTree is balanced" prop_balancedTreeBalanced
         ]
     ]
 
@@ -48,34 +63,71 @@ prop_setMultiInsert x = insert (insert empty x) x == insert empty x where
 
 prop_setMultiInsertContains :: [Int] -> Bool
 prop_setMultiInsertContains xs = all (member s) xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 prop_setMultiInsertOnlyContains :: [Int] -> Int -> Bool
 prop_setMultiInsertOnlyContains xs y = member s y == elem y xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 prop_setMultiInsertContainsMember' :: [Int] -> Bool
 prop_setMultiInsertContainsMember' xs = all (fst . member' s) xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 prop_setMultiInsertOnlyContainsMember' :: [Int] -> Int -> Bool
 prop_setMultiInsertOnlyContainsMember' xs y = (fst $ member' s y) == elem y xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 prop_setMultiInsertContainsBetterMember' :: [Int] -> Bool
 prop_setMultiInsertContainsBetterMember' xs = all (fst . betterMember' s) xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 prop_setMultiInsertOnlyContainsBetterMember' :: [Int] -> Int -> Bool
 prop_setMultiInsertOnlyContainsBetterMember' xs y = (fst $ betterMember' s y) == elem y xs where
-    s = setFromList xs
+    s = treeFromList xs
 
 case_depthWorst :: Assertion
-case_depthWorst = depth (setFromList [1..20] :: Tree Int) @?= 20
+case_depthWorst = depth (treeFromList [1..20] :: Tree Int) @?= 20
+
+case_depth2 :: Assertion
+case_depth2 = depth (treeFromList [2,1,3,4] :: Tree Int) @?= 3
 
 prop_depthWorst :: [Int] -> Bool
-prop_depthWorst xs = depth (setFromList xs) <= length xs
+prop_depthWorst xs = depth (treeFromList xs) <= length xs
 
 prop_betterMember'IsBetter :: [Int] -> Int -> Bool
 prop_betterMember'IsBetter xs x = (snd $ betterMember' s x) <= 2 * (depth s) + 1 where
-    s = setFromList xs
+    s = treeFromList xs
+
+prop_setMultiBetterInsertContains :: [Int] -> Bool
+prop_setMultiBetterInsertContains xs = all (member s) xs where
+    s = foldl betterInsert empty xs
+
+prop_setMultiBetterInsertOnlyContains :: [Int] -> Int -> Bool
+prop_setMultiBetterInsertOnlyContains xs y = member s y == elem y xs where
+    s = foldl betterInsert empty xs
+
+prop_sizeTreeFromList :: [Int] -> Bool
+prop_sizeTreeFromList xs = size (treeFromList xs) == length (nub xs)
+
+-- I don't want large depths, it takes too long. Also doesn't make sense for
+-- negative numbers
+completeInts :: Gen Int
+completeInts = choose (1, 20)
+
+prop_completeCorrectDepth :: Char -> Property
+prop_completeCorrectDepth x = forAll completeInts $ \d -> depth (complete x d) == d
+
+prop_completeCorrectSize :: Char -> Property
+prop_completeCorrectSize x = forAll completeInts $ \d -> size (complete x d) == 2^d - 1
+
+balancedTreeInts :: Gen Int
+balancedTreeInts = choose (1, 100000)
+
+prop_balancedTreeCorrectSize :: Char -> Property
+prop_balancedTreeCorrectSize x = forAll balancedTreeInts $ \s -> size (balancedTree x s) == s
+
+prop_balancedTreeBalanced :: Char -> Property
+prop_balancedTreeBalanced x = forAll balancedTreeInts $ impl where
+    impl s = abs ((size l) - (size r)) <= 1 where
+        (T l _ r) = balancedTree x s
+
