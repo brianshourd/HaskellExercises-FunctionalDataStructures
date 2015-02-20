3318c67eb7fa50823cc951f11999d02edfecb3ad
Brian Shourd <brian.shourd@gmail.com>
2015-02-19 22:02:13 -0800

    Exercise 3.6: implement binomial heap without redundant rank tracking

--- src/Chapter3/BinomialHeap.hs
+++ src/Chapter3/BinomialHeap.hs
@@ -1,48 +1,49 @@
 module Chapter3.BinomialHeap where
 
 -- Get set up with the basics from the book
-data Tree a = Node Int a [Tree a]
+-- Exercise 3.6 - reimplement without rank on each Tree
+data Tree a = Node a [Tree a]
     deriving (Show, Eq)
 
 root :: Tree a -> a
-root (Node _ x _) = x
+root (Node x _) = x
 
 rank :: Tree a -> Int
-rank (Node r _ _) = r
+rank (Node _ ts) = length ts
 
 trees :: Tree a -> [Tree a]
-trees (Node _ _ ts) = ts
+trees (Node _ ts) = ts
 
 link :: (Ord a) => Tree a -> Tree a -> Tree a
-link t1@(Node r1 x1 ts1) t2@(Node _ x2 ts2) =
-    if (x1 <= x2) then Node (r1 + 1) x1 (t2:ts1)
-    else Node (r1 + 1) x2 (t1:ts2)
+link t1@(Node x1 ts1) t2@(Node x2 ts2) =
+    if (x1 <= x2) then Node x1 (t2:ts1)
+    else Node x2 (t1:ts2)
 
-type Heap a = [Tree a]
+type Heap a = [(Int, Tree a)]
 
 insTree :: (Ord a) => Heap a -> Tree a -> Heap a
-insTree [] t = [t]
-insTree h@(t:ts) t' =
-    if rank t' < rank t then t' : h
+insTree [] t = [(rank t, t)]
+insTree h@((r,t):ts) t' =
+    if rank t' < r then (rank t',t') : h
     else insTree ts (link t t')
 
 insert :: (Ord a) => Heap a -> a -> Heap a
-insert h x = insTree h (Node 0 x [])
+insert h x = insTree h (Node x [])
 
 merge :: (Ord a) => Heap a -> Heap a -> Heap a
 merge h1 [] = h1
 merge [] h2 = h2
-merge h1@(t1:ts1) h2@(t2:ts2)
-    | rank t1 < rank t2 = t1 : (merge ts1 h2)
-    | rank t2 < rank t1 = t2 : (merge ts2 h1)
+merge h1@(rt1@(r1,t1):ts1) h2@(rt2@(r2,t2):ts2)
+    | r1 < r2 = rt1 : (merge ts1 h2)
+    | r2 < r1 = rt2 : (merge ts2 h1)
     | otherwise = insTree (merge ts1 ts2) (link t1 t2)
 
 removeMinTree :: (Ord a) => Heap a -> Maybe (Tree a, Heap a)
 removeMinTree [] = Nothing
-removeMinTree (t:[]) = Just (t, [])
-removeMinTree (t:ts) =
+removeMinTree ((_,t):[]) = Just (t, [])
+removeMinTree (rt@(r,t):ts) =
     if root t <= root t' then Just (t, ts)
-    else Just (t', t:ts) where
+    else Just (t', rt:ts) where
         Just (t', _) = removeMinTree ts
 
 findMin :: (Ord a) => Heap a -> Maybe a
@@ -51,9 +52,9 @@ findMin h = removeMinTree h >>= Just . root . fst
 deleteMin :: (Ord a) => Heap a -> Heap a
 deleteMin h = case removeMinTree h of
     Nothing -> []
-    Just ((Node _ _ ts1), ts2) -> merge (reverse ts1) ts2
+    Just ((Node _ ts1), ts2) -> merge (map (\t -> (rank t, t)) . reverse $ ts1) ts2
 
 -- Exercise 3.5: define findMin without a call to removeMinTree
 findMin' :: (Ord a) => Heap a -> Maybe a
 findMin' [] = Nothing
-findMin' h = Just . minimum . map root $ h
+findMin' h = Just . minimum . map (root . snd) $ h
--- tests/Chapter3/BinomialHeapTests.hs
+++ tests/Chapter3/BinomialHeapTests.hs
@@ -6,6 +6,7 @@ import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Test.HUnit
 import Test.QuickCheck
+import Data.Function (on)
 import Data.List (nubBy)
 
 import Chapter3.BinomialHeap
@@ -13,7 +14,8 @@ import Chapter3.BinomialHeap
 binomialHeapTestGroup :: Test.Framework.Test
 binomialHeapTestGroup = testGroup "Chapter 3 - BinomialHeaps"
     [testGroup "Basic binomial heap tests"
-        [testProperty "findMin actually finds minimum" prop_findMinFindsMin
+        [testProperty "stated rank matches actual rank" prop_rankIsTrue
+        ,testProperty "findMin actually finds minimum" prop_findMinFindsMin
         ,testProperty "deleteMin increases minimum" prop_deleteMinIncreases
         ,testCase "simple link test" case_simpleLinkTest
         ,testProperty "insTree maintains rank order" prop_insTreeRankOrder
@@ -30,6 +32,10 @@ heapFromList = foldl insert []
 isSorted :: (Ord a) => [a] -> Bool
 isSorted xs = all (\(x, y) -> x <= y) $ zip xs (tail xs)
 
+prop_rankIsTrue :: [Int] -> Bool
+prop_rankIsTrue xs = map fst h == map (rank . snd) h where
+    h = heapFromList xs
+
 prop_findMinFindsMin :: NonEmptyList Int -> Bool
 prop_findMinFindsMin (NonEmpty xs) = findMin (heapFromList xs) == Just (minimum xs)
 
@@ -42,16 +48,16 @@ prop_deleteMinIncreases :: Property
 prop_deleteMinIncreases = forAll (heapsBiggerThan 2) (\h -> findMin (deleteMin h) >= findMin h)
 
 case_simpleLinkTest :: Assertion
-case_simpleLinkTest = link (Node 1 1 [Node 0 2 []] :: Tree Int) (Node 1 3 [Node 0 4 []]) @?= (Node 2 1 [Node 1 3 [Node 0 4 []], Node 0 2 []])
+case_simpleLinkTest = link (Node 1 [Node 2 []] :: Tree Int) (Node 3 [Node 4 []]) @?= (Node 1 [Node 3 [Node 4 []], Node 2 []])
 
 hasRankOrder :: (Ord a) => Heap a -> Bool
-hasRankOrder = isSorted . map rank
+hasRankOrder = isSorted . map fst
 
 intTree :: Gen (Tree Int)
 intTree = do
     h <- heapsBiggerThan 1
     i <- choose (0, length h - 1)
-    return $ h !! i
+    return $ snd (h !! i)
 
 prop_insTreeRankOrder :: [Int] -> Property
 prop_insTreeRankOrder xs = forAll intTree (\t -> hasRankOrder (insTree h t)) where
@@ -61,7 +67,7 @@ prop_mergeRankOrder :: [Int] -> [Int] -> Bool
 prop_mergeRankOrder xs ys = hasRankOrder $ merge (heapFromList xs) (heapFromList ys)
 
 hasUniqueRank :: (Ord a) => Heap a -> Bool
-hasUniqueRank h = nubBy (\t1 t2 -> rank t1 == rank t2) h == h
+hasUniqueRank h = nubBy ((==) `on` fst) h == h
 
 prop_insTreeUniqRank :: [Int] -> Property
 prop_insTreeUniqRank xs = forAll intTree (\t -> hasUniqueRank (insTree h t)) where
